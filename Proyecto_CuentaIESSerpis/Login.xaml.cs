9b0994a10cad7916b382b7bdf3821852bf570d08
alibar92 <alibar@alu.iesserpis.org>
2022-02-05 22:29:29 +0100

    Agregar archivos de proyecto.

---
+++ Proyecto_CuentaIESSerpis/Login.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Proyecto_CuentaIESSerpis
+{
+    /// <summary>
+    /// Lógica de interacción para Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public bool UserOK { get; set; }
+        public char PasswordChar { get; set; }
+
+        public Login()
+        {
+            InitializeComponent();
+            GridRegistro.Visibility = Visibility.Hidden;
+            CalcHash(passwordBox.Password);
+        }
+        private string CalcHash(string contra)
+        {
+            UTF8Encoding enc = new UTF8Encoding();
+            byte[] data = enc.GetBytes(contra);
+            byte[] result;
+            SHA256CryptoServiceProvider sha = new SHA256CryptoServiceProvider();
+            result = sha.ComputeHash(data);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < result.Length; i++)
+            {
+                // Convertimos los valores en hexadecimal
+                // cuando tiene una cifra hay que rellenarlo con cero
+                // para que siempre ocupen dos dígitos.
+                if (result[i] < 16)
+                {
+                    sb.Append("0");
+                }
+                sb.Append(result[i].ToString("x"));
+            }
+            return sb.ToString().ToUpper();
+
+        }
+
+    }
+}
---
+++ Proyecto_CuentaIESSerpis/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Principal;
+
+namespace Proyecto_CuentaIESSerpis
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+
+            if (!CallLogin())
+            {
+                Application.Current.Shutdown(-1);
+            }
+            InitializeComponent();
+            //LogedUser.Content = System.Threading.Thread.CurrentPrincipal.Identity.Name;
+
+        }
+
+        private bool CallLogin()
+        {
+            Login login = new Login();
+            login.ShowDialog();
+
+            if (login.UserOK)
+            {
+                //Para trabajar con usuarios que no son de Windows, sino de la BBDD
+                AppDomain.CurrentDomain.SetPrincipalPolicy(PrincipalPolicy.UnauthenticatedPrincipal);
+                //Crear una identidad a partir del usuario
+                IIdentity identity = new GenericIdentity(login.usuarioTxtBox.Text, "Database");
+                string[] roles = { "Docente", "Alumno" };
+                GenericPrincipal credencial = new GenericPrincipal(identity, roles);
+                //Asignar esta credencial a la aplicación
+                System.Threading.Thread.CurrentPrincipal = credencial;
+
+                if(roles.Equals("Docente")) { //si es docente
+                    GridAlumno.Visibility = Visibility.Hidden; //se esconde el Grid del alumno
+                }
+                else //si el alumno
+                {
+                    GridDocente.Visibility = Visibility.Hidden; //se esconde el Grid docente
+                }
+
+            }
+            return true;
+        }
+    }
+}
